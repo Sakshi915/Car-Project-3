32f7ca9e9a013fd2173cb52d0a1199f66ada02c5
Sakshi Mohite <sakshim3@kpit.com>
2024-01-16 12:12:41 +0530

    Second_Commit

---
+++ carsspringbootproject/target/classes/com/example/carsspringbootproject/Layer1/Cars.sql
@@ -0,0 +1,10 @@
+create table cars(
+    carNo int,
+    carName varchar(20),
+    carPrice int
+);
+Insert into cars values (1,"Vw",100);
+Insert into cars values (2,"wagnor",200);
+Insert into cars values (3,"Celenio",300);
+Insert into cars values (4,"car1",400);
+Insert into cars values (5,"car2",500);
